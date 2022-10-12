7834aac9937222f48d3eb145c248cac6e464b585
Shaurya Omar <64625017+Shoye21@users.noreply.github.com>
2022-10-12 19:42:45 +0530

    mobileNumber

---
+++ MobilePhone.java
@@ -0,0 +1,51 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+    private  String myNumber;
+    private ArrayList <Contact> myContact;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContact = new ArrayList<Contact>();
+    }
+
+    public boolean addNewContact(Contact contact){
+        if(findContact(contact.getName())>=0){
+            System.out.println("Contact is already on file");
+            return false;
+        }
+        myContact.add(contact);
+        return true;
+    }
+    private int findContact(Contact contact){
+        return this.myContact.indexOf(contact);
+    }
+    private int findContact(String contactName){
+        for (int i = 0; i < this.myContact.size(); i++) {
+            Contact contact = this.myContact.get(i);
+            if(contact.getName().equals(contactName)){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public boolean updateContact(Contact oldContact, Contact newContact){
+        int foundPosition = findContact(oldContact);
+        if(foundPosition <0){
+            System.out.println(oldContact.getName() + " not found");
+            return false;
+        }
+        this.myContact.set(foundPosition, newContact);
+        System.out.println(oldContact.getName() + " replaced with " + newContact.getName());
+        return true;
+    }
+
+    public String queryContact(Contact contact){
+        if(findContact(Contact)>=0){
+            return contact.getName();
+        }
+        return null;
+    }
+}
